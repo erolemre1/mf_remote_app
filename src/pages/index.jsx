341bb7cfcf6ef107d4deab7046d7e98721c60465
Emre Güleç <emre.gulec@defacto.com>
2025-07-09 16:49:16 +0300

    remote app ssr fixes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,60 +1,60 @@
 
 
 
-// // // // server kullanım yok state yok 
+// // // server kullanım yok state yok 
 
-// const React = require("react");
+const React = require("react");
 
-// function App() {
-//   return (
-//     <div className="grid min-h-screen p-8 pb-20">
-//       <main className="flex flex-col items-center">
-//         <img src="/next.svg" alt="Next.js logo" width="180" height="38" />
-//         <img src="/file.svg" alt="Next.js logo" width="180" height="38" />
-//         <div>
-//          JSX çalışıyor tüm uygulama clientta birleşiyor
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="grid min-h-screen p-8 pb-20">
+      <main className="flex flex-col items-center">
+        <img src="/next.svg" alt="Next.js logo" width="180" height="38" />
+        <img src="/file.svg" alt="Next.js logo" width="180" height="38" />
+        <div>
+         JSX çalışıyor tüm uygulama clientta birleşiyor 111
+        </div>
+      </main>
+    </div>
+  );
+}
 
-// module.exports = App;
+module.exports = App;
 
 
 // // // // //server kkullanım state management
 
-const React = require("react");
+// const React = require("react");
 
-function RemoteCounter() {
-  const [count, setCount] = React.useState(0);
-  const [username, setUsername] = React.useState('-');
+// function RemoteCounter() {
+//   const [count, setCount] = React.useState(0);
+//   const [username, setUsername] = React.useState('-');
 
-  React.useEffect(() => {
-    const increment = () => setCount(c => c + 1);
-    window.addEventListener("remote-increment", increment);
+//   React.useEffect(() => {
+//     const increment = () => setCount(c => c + 1);
+//     window.addEventListener("remote-increment", increment);
 
-    console.log("remote-app gelddii 11");
+//     console.log("remote-app gelddii 11");
 
-    const handleData = (event) => {
-      console.log("Remote: veri", event.detail);
-      setUsername(event.detail.username);
-    };
-    window.addEventListener("remote-data", handleData);
+//     const handleData = (event) => {
+//       console.log("Remote: veri", event.detail);
+//       setUsername(event.detail.username);
+//     };
+//     window.addEventListener("remote-data", handleData);
 
-    return () => {
-      window.removeEventListener("remote-increment", increment);
-      window.removeEventListener("remote-data", handleData);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener("remote-increment", increment);
+//       window.removeEventListener("remote-data", handleData);
+//     };
+//   }, []);
 
-  return React.createElement(
-    "div",
-    { id: "remote-root" },
-    `Sayaç dee: ${count}`,
-    React.createElement("div", null, `Kullanıcı 2222: ${username ?? "-"}`)
-  );
-}
+//   return React.createElement(
+//     "div",
+//     { id: "remote-root" },
+//     `Sayaç dee: ${count}`,
+//     React.createElement("div", null, `Kullanıcı 2222: ${username ?? "-"}`)
+//   );
+// }
 
-module.exports = RemoteCounter;
+// module.exports = RemoteCounter;
 
