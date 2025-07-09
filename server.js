40c07465fe07a11d4066dfa170488e3effd9e692
Emre Güleç <emre.gulec@defacto.com>
2025-07-09 17:44:06 +0300

    remote app change ssr support

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ app.get('/microfrontend', (req, res) => {
   `);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}/microfrontend`);
 });
